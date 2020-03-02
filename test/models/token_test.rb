5c27b92f870fe59ed8223e6a37229f1004073ae9
Jibran Kalia <jibrankalia@jibrans-mbp.home>
2020-03-02 16:20:26 -0600

    Adding tokens table

---
+++ test/models/token_test.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: tokens
+#
+#  id         :bigint           not null, primary key
+#  word       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_tokens_on_word  (word)
+#
+require 'test_helper'
+
+class TokenTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
